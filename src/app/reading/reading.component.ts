0798548648354780b0215e6caf1f79aa4db798ee
Hannah Morgan <hannahdreyer@gmail.com>
2020-08-12 16:35:11 +0200

    Reading Videos, Layout, Functionality added
    All reading videos for K-5 are linked, layout adjusted, scroll to top button added, side nagivation added

--- src/app/reading/reading.component.ts
+++ src/app/reading/reading.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-reading',
-  templateUrl: './reading.component.html',
-  styleUrls: ['./reading.component.css']
+  selector: "app-reading",
+  templateUrl: "./reading.component.html",
+  styleUrls: ["./reading.component.css"],
 })
 export class ReadingComponent implements OnInit {
+  scrolled = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  @HostListener("window:scroll", ["$event"]) onScrollEvent($event) {
+    const number = window.pageYOffset;
+    if (number >= 50) {
+      this.scrolled = 1;
+    } else {
+      this.scrolled = 0;
+    }
+    console.log($event, "Page Y-axis offset by:" + window.pageYOffset);
   }
 
+  goToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  // scroll() {
+  //   document
+  //     .querySelector("#target")
+  //     .scrollIntoView({ behavior: "smooth", block: "center" });
+  // }
+
+  // scroll(el: HTMLElement) {
+  //   el.scrollIntoView();
+  // }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
