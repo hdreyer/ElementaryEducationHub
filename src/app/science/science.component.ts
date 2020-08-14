e6b8583669a8c3e182710b48fc82f7ad97b195b3
Hannah Morgan <hannahdreyer@gmail.com>
2020-08-14 21:11:56 +0200

    Scroll to top functionality
    Added scroll to top functionality to Science and Math's TS files...still need button structure on template

--- src/app/science/science.component.ts
+++ src/app/science/science.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-science',
-  templateUrl: './science.component.html',
-  styleUrls: ['./science.component.css']
+  selector: "app-science",
+  templateUrl: "./science.component.html",
+  styleUrls: ["./science.component.css"],
 })
 export class ScienceComponent implements OnInit {
+  scrolled = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  @HostListener("window:scroll", ["$event"]) onScrollEvent($event) {
+    const number = window.pageYOffset;
+    if (number >= 50) {
+      this.scrolled = 1;
+    } else {
+      this.scrolled = 0;
+    }
+    // console.log($event, "Page Y-axis offset by:" + window.pageYOffset);
   }
 
+  goToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
