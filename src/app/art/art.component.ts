d3265d9944afebba42f50c8c5e706b4e032d44d1
Hannah Morgan <hannahdreyer@gmail.com>
2020-08-20 16:55:01 +0200

    Art page created
    Added all the art videos, scroll up page button, and side navigation

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-art',
-  templateUrl: './art.component.html',
-  styleUrls: ['./art.component.css']
+  selector: "app-art",
+  templateUrl: "./art.component.html",
+  styleUrls: ["./art.component.css"],
 })
 export class ArtComponent implements OnInit {
+  scrolled = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  @HostListener("window:scroll", ["$event"]) onScrollEvent($event) {
+    const number = window.pageYOffset;
+    if (number >= 50) {
+      this.scrolled = 1;
+    } else {
+      this.scrolled = 0;
+    }
+    // console.log($event, "Page Y-axis offset by:" + window.pageYOffset);
   }
 
+  goToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
