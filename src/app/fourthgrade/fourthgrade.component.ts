5fb5bdce5cfae275cd2e98b625b7a34c054f03c7
Hannah Morgan <hannahdreyer@gmail.com>
2020-08-25 13:43:50 +0200

    Fourth Grade Component finished

--- src/app/fourthgrade/fourthgrade.component.ts
+++ src/app/fourthgrade/fourthgrade.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-fourthgrade',
-  templateUrl: './fourthgrade.component.html',
-  styleUrls: ['./fourthgrade.component.css']
+  selector: "app-fourthgrade",
+  templateUrl: "./fourthgrade.component.html",
+  styleUrls: ["./fourthgrade.component.css"],
 })
 export class FourthgradeComponent implements OnInit {
+  scrolled = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  @HostListener("window:scroll", ["$event"]) onScrollEvent($event) {
+    const number = window.pageYOffset;
+    if (number >= 50) {
+      this.scrolled = 1;
+    } else {
+      this.scrolled = 0;
+    }
   }
 
+  goToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
